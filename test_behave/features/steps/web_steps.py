32aa51fcb79572a87cc6d8d9829024ed61710974
lukashudec <lukas.hudec@centrum.sk>
2021-02-01 18:07:53 +0100

    Added base for Web testing

--- test_behave/features/steps/web_steps.py
+++ test_behave/features/steps/web_steps.py
@@ -1,23 +1,33 @@
 from behave import given, then, when
 from hamcrest import *
 from selenium import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 page = 'https://www.duckduckgo.com'
-driver = webdriver.Firefox()
+driver = webdriver.Chrome('C:/Users/lenovo/Downloads/chromedriver_win32_87/chromedriver.exe')
 driver.implicitly_wait(3)
+
 
 @given('I am on the {page} homepage')
 def step(context, page):
     driver.get(page)
 
-    driver.switch_to.frame(driver.find_element_by_xpath("//iframe[contains(@src, 'consent.google.com')]"))
-    driver.find_element_by_xpath('//*[@id="introAgreeButton"]/span/span').click()
+    try:
+        driver.switch_to.frame(driver.find_element_by_xpath("//iframe[contains(@src, 'consent.google.com')]"))
+        driver.find_element_by_xpath('//*[@id="introAgreeButton"]/span/span').click()
+    except:
+        pass
+
 
 @when('I enter search term: {search}')
 def step(context, search):
     element = driver.find_element(By.NAME, "q")
-    element.send_keys("search")
+    element.send_keys(search)
+
 
 @then('Search results for {search_result} should appear')
 def step(context, search_result):
+    time.sleep(5)
     driver.close()
--- test_behave/features/web_test.feature
+++ test_behave/features/web_test.feature
@@ -1,6 +1,6 @@
 @web_test
 Feature: LambdaTest search
-  Scenario: Search for LambdaTest on DuckDuckGo
-    Given I am on the DuckDuckGo homepage
+  Scenario: Search for google on google
+    Given I am on the https://www.google.com homepage
     When I enter search term: google
     Then Search results for google should appear
