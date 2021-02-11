31efb541f49937034bd2ffb814c611b197f9f219
lukashudec <lukas.hudec@centrum.sk>
2021-02-11 18:39:37 +0100

    trying new idea for custom WebElement  adding optional return_page  if defined, it will be returned on welement.click() after perfoming super.click

--- test_behave/features/utility/welement.py
+++ test_behave/features/utility/welement.py
@@ -4,13 +4,19 @@ from selenium.webdriver.remote.webelement import WebElement
 class welement(WebElement):
     driver = None
 
-    def __init__(self, loc_type, loc_str):
+    def __init__(self, loc_type, loc_str, return_page = None):
         self.loc_type = loc_type
         self.loc_str = loc_str
+        self.return_page = return_page
 
     def __get__(self) -> WebElement:
         return self.driver.find_element(self.loc_type, self.loc_str)
 
+    def click(self):
+        if self.return_page is not None:
+            super.click()
+            return self.return_page
+        return super.click()
 
 some = welement('xpath', '...')
 
