a59b23d9d4e53d9818abd77df13114d08f2e9016
lukashudec <lukas.hudec@centrum.sk>
2021-02-22 20:18:00 +0100

    adding test_runner.py as a command line replacement fixes for FindBy caching

---
+++ testRunner.py
@@ -0,0 +1,22 @@
+def run_unit_test_with_coverage():
+    import pytest
+    pytest.main(["-x", "test_unit/test_calendar_class.py",
+                 '--html-report', './report/pytest',
+                 '--verbose',
+                 '--disable-warnings',
+                 '--cov-report', 'html:./report/coverage',
+                 '--cov', 'Calendar'])
+
+
+def run_end2end():
+    import pytest
+    pytest.main(["-x", "test_e2e/tests.py",
+                 '--html-report', './report/pytest',
+                 '--verbose',
+                 '--disable-warnings',])
+
+
+
+'''run_unit_test_with_coverage()'''
+run_end2end()
+
